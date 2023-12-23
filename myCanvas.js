4c940f04dbdab45333d2a861cd4a38a872222167
Edgar Dimas Isaadrazak <edgar.dimas.ir@gmail.com>
2023-12-23 18:48:52 +0700

    add paddle feat.

--- myCanvas.js
+++ myCanvas.js
@@ -1,10 +1,26 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 let ballRadius = 10;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
 let x = canvas.width - 25;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+
+let rightPressed = false;
+let leftPressed = false;
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+/**
+ * in the "addEventListener" method in JavaScript, the first argument represents the type of event
+ * for which you want to listen. The event types are specified as strings.
+ *
+ * for example:
+ * "keydown" is a string representing the event that occurs when a key on the keyboard is pressed down
+ * "keyup" is a string representing the event that occurs when a key on the keyboard is released after being pressed.
+ */
 
 function drawBall() {
   ctx.beginPath(); // Start a new path
@@ -19,16 +35,42 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function keyDownHandler(e) {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    rightPressed = true;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key === "left" || e.key === "ArrowLeft") {
+    leftPressed = false;
+  }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) dx = -dx;
   if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) dy = -dy;
 
   x += dx;
   y += dy;
 
-  console.log("x >>>>>>>> ", x);
+  if (rightPressed) paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+  else if (leftPressed) paddleX = Math.max(paddleX - 7, 0);
 }
 
 setInterval(draw, 10);
